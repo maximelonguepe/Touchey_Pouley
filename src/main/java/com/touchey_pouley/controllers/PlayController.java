3eb40c0e4634e375cdcec8c5b35f28f805c1ca2f
maximelonguepe <longuepemaxime73@gmail.com>
2022-10-13 23:21:35 +0200

    ajout verif si la partie a été lancée ou non

--- src/main/java/com/touchey_pouley/controllers/PlayController.java
+++ src/main/java/com/touchey_pouley/controllers/PlayController.java
@@ -1,10 +1,25 @@
 package com.touchey_pouley.controllers;
 
+import com.touchey_pouley.game.Game;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("play")
 public class PlayController {
 
+    @GetMapping(path = "")
+    public ResponseEntity getAgencyByIdOrName(@PathParam("x") Integer x, @PathParam("y") Integer y) {
+        Game game= Game.getInstance();
+        if(!game.isStarted()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Les poulets n'ont pas encore trouvé leur place");
+        }
+        return new ResponseEntity(game,HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/touchey_pouley/game/Game.java
+++ src/main/java/com/touchey_pouley/game/Game.java
@@ -1,21 +1,30 @@
 package com.touchey_pouley.game;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.touchey_pouley.game.poulets.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
+@Getter
 public class Game {
+    @JsonIgnore
+    private boolean started;
+    @JsonIgnore
     private static Game instance;
     private  List<Coordonnees> playedCoordinates;
+    @JsonIgnore
     private List<Coordonnees> pouletPositions;
     private List<Poulet> pouletList;
 
     public void startGame(){
+        started=true;
         pouletPositions =new ArrayList<>();
         pouletList=new ArrayList<>();
+        playedCoordinates=new ArrayList<>();
         pouletList.add(PouleTony.getInstance());
         pouletList.add(PouleThierry.getInstance());
         pouletList.add(PouleThibault.getInstance());
